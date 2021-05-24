71007068aa97b17a45756b4ef0d4d7fcd9d2120f
Jean-Baptiste Despujol <jean-baptiste.despujol@epita.fr>
2021-05-24 23:08:24 +0200

    new test

--- app/src/test/java/com/example/inventoryexpiry/MethodsTest.java
+++ app/src/test/java/com/example/inventoryexpiry/MethodsTest.java
@@ -6,13 +6,23 @@ import java.util.ArrayList;
 
 public class MethodsTest extends TestCase {
 
-    public void testAddToList() {
-        ArrayList<Product> list = new ArrayList<>();
-        Product p1 = new Product("12/08/2022", "1234567891234");
-        Product p2 = new Product("12/08/2021", "1234567891234");
+    ArrayList<Product> list = new ArrayList<>();
+    Product p1 = new Product("12/08/2022", "1234567891234");
+    Product p2 = new Product("12/08/2021", "1234567891234");
+    Product p3 = new Product("12/08/2021", "9876543210987");
+
+    public void testAddToListIsReplaced() {
         Methods.addToList(list, p1);
         Methods.addToList(list, p2);
         assertEquals(list.toArray().length, 1);
         assertEquals(list.get(0).getExpiryDate(), "12/08/2021");
     }
+
+
+    public void testAddToListDifferentEntries() {
+        Methods.addToList(list, p1);
+        Methods.addToList(list, p3);
+        assertEquals(list.toArray().length, 2);
+    }
+
 }
